7fb95711cce5d62e70a9c430b511925355121a0a
Es-dot <67278894+Es-dot@users.noreply.github.com>
2020-07-20 18:59:40 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -12,5 +12,5 @@ data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
 # create histogram 
 hist(data1$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-png("plot1.png", width=480, height=480)
+png("Plot1.png", width=480, height=480)
 dev.off()
