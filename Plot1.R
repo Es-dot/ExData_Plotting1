1ba41c19835e36349666da92682a5d8313e2b7cc
Es-dot <67278894+Es-dot@users.noreply.github.com>
2020-07-20 18:47:30 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -5,7 +5,7 @@ setwd("Documents/R/Getting & cleaning data - Coursera")
 household <- read_delim("Documents/R/Getting & cleaning data - Coursera/household_power_consumption.txt", 
                          ";", escape_double = FALSE, trim_ws = TRUE)
 
-# set the right date format
+# set to the right date format and to the needed dates
 data1 <- subset(household, Date %in% c("1/2/2007","2/2/2007"))
 data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
 
