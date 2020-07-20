7caffc7075eb626212b651a47665d36d44b263d7
Es-dot <67278894+Es-dot@users.noreply.github.com>
2020-07-20 18:41:20 +0200

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,29 @@
+# Set the directory to save the plots
+setwd("Documents/R/Getting & cleaning data - Coursera")
+
+# import dataset
+household <- read_delim("Documents/R/Getting & cleaning data - Coursera/household_power_consumption.txt", 
+                        +     ";", escape_double = FALSE, trim_ws = TRUE)
+
+# subset dataset to the needed dates
+data1 <- subset(household, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(data1$Date), data1$Time)
+data1$Datetime <- as.POSIXct(datetime)
+
+# plot
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data1, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
